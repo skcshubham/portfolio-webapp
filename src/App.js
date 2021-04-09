7943861fe7eaff8232a9f419f5997984c6a5c5ec
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-04-09 17:41:44 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
-import Education from "./Components/Education/Education";
 import Navbar from "./Components/Navbar/Navbar";
+import Education from "./Components/Education/Education";
+import About from "./Components/About/About";
+import Projects from "./Components/Projects/Projects";
+import Experience from "./Components/Experience/Experience";
 
 function App() {
 	return (
@@ -14,6 +17,9 @@ function App() {
 				<Switch>
 					<Route exact path="/" component={Home} />
 					<Route exact path="/education" component={Education} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/projects" component={Projects} />
+					<Route exact path="/experience" component={Experience} />
 				</Switch>
 				<Footer />
 			</div>
---
+++ src/Components/About/About.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./About.css";
+import cpp from "./C++.png";
+
+const About = () => {
+	return (
+		<div className="skills about">
+			<h1>About Me</h1>
+			<br />
+			<div className="content">
+				<div className="section">
+					<h2 className="section-header">Summary</h2>
+					<p>
+						I am an undergraduate student with an innate love for
+						problem-solving with algorithms and a passion for frontend
+						development. I'm a B.Tech Pre-Final year Computer Science student
+						from SRM IST, Chennai with strong core computer science knowledge of
+						Operating Systems, Database Management System and Data Structures
+						and Algorithms.
+					</p>
+				</div>
+			</div>
+			<div className="section">
+				<h2 className="section-header">Skills</h2>
+
+				<img src={cpp} alt="C++ logo" style={{ width: "68px" }} />
+				<i className="fab fa-html5 fa-5x"></i>
+				<i className="fab fa-css3 fa-5x"></i>
+				<i className="fab fa-bootstrap fa-5x"></i>
+				<i className="fab fa-node-js fa-5x"></i>
+				<i className="fab fa-react fa-5x"></i>
+				<i className="fas fa-database fa-5x"></i>
+				<i className="fas fa-terminal fa-4x"></i>
+				<i className="fab fa-ubuntu fa-5x"></i>
+			</div>
+			<div className="section">
+				<h2 className="section-header">Achievements</h2>
+				<ul>
+					<li>
+						14th rank among 2200+ students on Geeksforgeeks coding section in
+						the entire university.{" "}
+						<a
+							href="https://auth.geeksforgeeks.org/user/skcshubham/practice/"
+							style={{ color: "blue" }}
+						>
+							Link to profile
+						</a>
+					</li>
+					<li>
+						Solved over 200+ questions on Data Structures and Algorithms on
+						Leetcode.{" "}
+						<a
+							href="https://leetcode.com/skcshubham/"
+							style={{ color: "blue" }}
+						>
+							Link to profile
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default About;
