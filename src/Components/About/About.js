1c36b7018aff714bef9d81d5ad59340fcd5dc59c
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-05-05 15:47:32 +0530

    a\changed logos

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./About.css";
-import cpp from "./C++.png";
 
 const About = () => {
 	return (
@@ -20,14 +19,36 @@ const About = () => {
 			</div>
 			<div className="section">
 				<h2 className="section-header">Skills</h2>
-				<img src={cpp} alt="C++ logo" style={{ width: "68px" }} />
-				<i className="fab fa-html5 fa-5x"></i>
-				<i className="fab fa-css3 fa-5x"></i>
-				<i className="fab fa-node-js fa-5x"></i>
-				<i className="fab fa-react fa-5x"></i>
-				<i className="fas fa-database fa-5x"></i>
-				<i className="fab fa-github fa-5x"></i>
-				<i className="fas fa-terminal fa-4x"></i>
+				<img
+					src="https://img.icons8.com/color/95/000000/c-plus-plus-logo.png"
+					alt="logo"
+				/>
+				<img
+					src="https://img.icons8.com/color/96/000000/html-5--v1.png"
+					alt="logo"
+				/>
+				<img src="https://img.icons8.com/color/96/000000/css3.png" alt="logo" />
+				<img
+					src="https://img.icons8.com/color/96/000000/javascript.png"
+					alt="logo"
+				/>
+				<img
+					src="https://img.icons8.com/color/96/000000/react-native.png"
+					alt="logo"
+				/>
+				<img
+					src="https://img.icons8.com/color/96/000000/redux.png"
+					alt="logo"
+				/>
+				<img
+					src="https://img.icons8.com/nolan/96/api-settings.png"
+					alt="logo"
+				/>
+				<img
+					src="https://img.icons8.com/plasticine/100/000000/oracle-pl-sql--v3.png"
+					alt="logo"
+				/>
+				<img src="https://img.icons8.com/color/96/000000/git.png" alt="logo" />
 			</div>
 			<div className="section">
 				<h2 className="section-header">Achievements</h2>
